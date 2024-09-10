3e08286b92e3bd27df92c39464668a8eac87b989
NatanFBagatoli <NatanFBagatoli@gmail.com>
2024-09-10 01:38:34 -0300

    primeiro commit

---
+++ TRABALHOFRONT/src/app.tsx
@@ -0,0 +1,25 @@
+export function App() {
+
+  return (
+<div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6 text-center">
+          <p className="text-zinc-300 text-lg">Login</p>
+          
+            <div>
+              </div>
+              <p className="text-zinc-300 text-lg">
+                <div className="h-16 bg-zinc-900 px-4 rounded-xl">
+              <input type="text" placeholder="Usuário"/>
+              <input type="text" placeholder="Senha"/> 
+                
+              <button>Entrar</button>
+              </div>
+              </p>
+            </div>
+          </div>
+        
+       
+       
+      )
+}
+
